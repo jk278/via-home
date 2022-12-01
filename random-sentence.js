f7a3200857da28f2b6f3af7305a0a3f38881f29c
jk278 <53921841+jk278@users.noreply.github.com>
2022-12-02 03:48:49 +0800

    Update random-sentence.js

--- random-sentence.js
+++ random-sentence.js
@@ -1,13 +1,13 @@
 /* ----- 短句显示的长度 ------ */
 /* ------- 长句显示的长度 ------- */
 /* --------- 长句显示的长度 -------- */
-var sentences=new Array(
-"✎...  偷得浮生半日闲～",
-"✎...  行到水穷处，坐看云起时",
-"✎...  醉后不知天在水，满船清梦压星河" ,
-"✎...  人生天地间，忽如远行客",
-"✎...  晚来天欲雪，能饮一杯无？",
-"✎...  风住尘香花已尽，日晚倦梳头",
+var sentences = new Array(
+    "✎...  偷得浮生半日闲～",
+    "✎...  行到水穷处，坐看云起时",
+    "✎...  醉后不知天在水，满船清梦压星河",
+    "✎...  人生天地间，忽如远行客",
+    "✎...  晚来天欲雪，能饮一杯无？",
+    "✎...  风住尘香花已尽，日晚倦梳头",
 );
 
 var randomSentence = sentences[Math.floor(Math.random() * sentences.length)];
@@ -18,12 +18,12 @@ function changeSize(sizeString, heightStr) {
     var style = document.createElement('style');
     document.head.appendChild(style);
     sheet = style.sheet;
-    sheet.addRule('input::placeholder', 'font-size:'+sizeString+';transform: translateY('+heightStr+')');
+    sheet.addRule('input::placeholder', 'font-size:' + sizeString + ';transform: translateY(' + heightStr + ')');
 }
 
-if (19<randomSentence.length && randomSentence.length<=21) {
+if (19 < randomSentence.length && randomSentence.length <= 21) {
     changeSize("13px", "-2px");
 }
-else if (21<randomSentence.length) {
+else if (21 < randomSentence.length) {
     changeSize("12px", "-3px");
 }
