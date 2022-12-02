9920a2fe1417940b612cace0de4e8552f289a110
jk278 <g1169861954@gmail.com>
2022-12-03 07:08:35 +0800

    .

--- js/longpress-custom.js
+++ js/longpress-custom.js
@@ -23,8 +23,15 @@ btn.addEventListener("touchstart", touchstartHander, false);
 btn.addEventListener("touchmove", touchmoveHander, false);
 btn.addEventListener("touchend", touchendHander, false);
 
-var p = document.getElementById("custom-text");
-a = document.getElementById("custom");
+// let和var的区别体现在作用域上。var的作用域被规定为一个函数作用域，
+// 而let则被规定为块作用域，块作用域要比函数作用域小一些，
+// 但是如果两者既没在函数中，也没在块作用域中定义，那么两者都属于全局作用域。
+// var允许在同一作用域中声明同名的变量，而let不可以。
+// es6中还有一个声明变量的命令const，const和let都是在声明的块作用域中有效，
+// 但是let声明的变量可变，值和类型都可以改变，没有限制。const声明额变量不能改变，
+// 所以，const一旦声明一个变量，就必须马上初始化，不能留到以后赋值。
+let p = document.getElementById("custom-text"),
+    a = document.getElementById("custom");
 
 changeSite(p, a);
 
