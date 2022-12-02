e534264bd4c4d00b60c3d9d702df81291323a917
jk278 <g1169861954@gmail.com>
2022-12-03 06:06:38 +0800

    。

--- js/longpress-custom.js
+++ js/longpress-custom.js
@@ -31,20 +31,23 @@ changeSite(p, a);
 function LongPress(p, a) {
     // alert("长按被触发");
     var name = window.prompt("请输入网站名：");
-    if (name != null) localStorage.setItem('name', name);
-    // 在主 html 读取存储 (不是)
-    if (name != null) {
+    if (name) {
+        // local storage 是同步的！
+        localStorage.setItem('name', name);
+        // 在主 html 读取存储 (不是)
         var site = window.prompt("请输入网址：");
-        if (site != null) localStorage.setItem('site', site);
+        if (site) {
+            localStorage.setItem('site', site);
+            changeSite(p, a);
+        }
     }
-    changeSite(p, a);
 }
 
 function changeSite(p, a) {
     var nameCache = localStorage.getItem("name");
     siteCache = localStorage.getItem("site");
 
-    if (nameCache != null && siteCache != null) {
+    if (nameCache && siteCache) {
         p.innerHTML = nameCache;
         a.setAttribut.href = siteCache;
     }
